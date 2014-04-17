1ba85f9330a81488e00042535aaeb5f9adc0aed5
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-04-17 16:30:27 +0200

    add stats for sidebar widgets

--- assets/javascripts/statistics.js
+++ assets/javascripts/statistics.js
@@ -39,3 +39,27 @@
         }.on('willDestroyElement')
     });
 })();
+
+(function () {
+    if (!Discourse.SidebarView) return; // this is the sidebar feature
+
+    var StatisticsView = Ember.View.extend({
+        templateName: "statistics",
+        tagName: "span",
+        noteDraft: "",
+        classNames: ['statistics-ui'],
+
+        loadStatistics: function() {
+            if (this.get("statistics")) return;
+            this.set('loading', true);
+            Discourse.ajax("/statistics/load", {}).then(function(receivedJSON) {
+                    this.set('loading', false);
+                    this.set("statistics", receivedJSON.statistics);
+                    this.rerender();
+            }.bind(this));
+        }.on("didInsertElement")
+    });
+
+Discourse.SidebarView.reopen({stats: StatisticsView.create()});
+
+})();
