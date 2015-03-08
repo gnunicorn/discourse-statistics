49469f1590a7559d1801fe00e03a42975de9d555
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-03-08 14:33:36 +0000

    add moderator statistic emails

--- integrate.rb
+++ integrate.rb
@@ -30,3 +30,47 @@ module Jobs
   end
 
 end
+
+class ModsViewsEmail < ActionMailer::Base
+  default charset: 'UTF-8'
+
+  include Email::BuildEmailHelper
+
+  def generate(stats)
+    build_email(SiteSetting.report_mods_email,
+                :template => 'moderator_stats',
+                :stats => stats)
+  end
+end
+
+module Jobs
+  class ModeratorStatistics < Jobs::Scheduled
+    every 2.weeks
+    sidekiq_options retry: false
+
+    def execute(args)
+      return if !SiteSetting.report_mods_email
+
+      view_query = TopicViewItem
+
+      if !SiteSetting.report_mods_category
+        cat_query = Category.get_ids_of_all_subs(SiteSetting.report_mods_category)
+        view_query = view_query.where("topic_id in (SELECT id FROM topics WHERE category_id IN (#{cat_query}))")
+      end
+
+      stats = User.where(:moderator => true).all.map do |user|
+        viewItem = view_query.where(:user_id => user.id).order('viewed_at DESC').first
+        if viewItem
+          topic = Topic.find(viewItem.topic_id)
+          " -  #{user.name} (#{user.username}) : #{viewItem.viewed_at} (#{topic.title})"
+        else
+          " -  #{user.name}(#{user.username}): UNKNOWN"
+        end
+      end
+      mail = ModsViewsEmail.generate(stats.join("\n"))
+      Email::Sender.new(mail, :moderator_mails).send
+    end
+
+  end
+
+end
